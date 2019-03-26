1e15fc637acb1c7c160602875eab2d1baeef3d7a
Edward2017 <lqhnuaa@163.com>
2019-03-26 16:41:15 +0800

    Update thermostat.f90

--- thermostat.f90
+++ thermostat.f90
@@ -112,8 +112,109 @@ end subroutine shift
 subroutine autocorrelation(temper,N,coefficient)
 implicit none
 integer *4 N,i,j,k
-real *8 temper(2*N),coefficient
+real *8 temper(2*N),coefficient，avg,dsig
+
+avg=0.0d0
+do i=1,2*N
+  avg=avg+temper(i)/(2*N)
+enddo
+dsig=0.0d0
+do i=1,2*N
+	dsig=dsig+(temper(i)-avg)**2
+enddo
 do i=1,N
+	coefficient=coefficient+(temper(i)-avg)*(temper(i+N)-avg)/dsig
+enddo
+return
+end subroutine autocorrelation
+
 	
+subroutine get_kinetic_energy
+use inputdata,only:pbead,mass,ngas,nbeads,Ek
+implicit none 
+integer *4 i,j,k
+Ek = 0.0d0
+do i = 1, 3
+  do j = 1, Ngas
+    do k = 1, Nbeads
+      Ek=Ek+0.5d0*pbead(i,j,k)*pbead(i,j,k)/mass(j)
+    end do
+  end do
+end do	
+
+end subroutine get_kinetic_energy
+
+
+subroutine get_Ering_energy
+use inputdata
+use parameters
+implicit none
+real *8 dx,dy,dz,wn
+integer *4 i,j,k
+Ering = 0.0d0
+dx=0.0d0
+dy=0.0d0
+dz=0.0d0
+wn = Nbeads / beta
+do j = 1, Ngas
+  dx = q(1,j,1) - q(1,j,Nbeads)
+  dy = q(2,j,1) - q(2,j,Nbeads)
+  dz = q(3,j,1) - q(3,j,Nbeads)
+  Ering=Ering+0.5d0*mass(j)*wn*wn*(dx*dx+dy*dy+dz*dz)
+
+  do k = 2, Nbeads
+    dx = q(1,j,k-1) - q(1,j,k)
+    dy = q(2,j,k-1) - q(2,j,k)
+    dz = q(3,j,k-1) - q(3,j,k)
+    Ering=Ering+0.5d0*mass(j)*wn*wn*(dx*dx+dy*dy+dz*dz)
+  end do
+end do
+
+end subroutine get_Ering_energy
+
+subroutine get_total_Ep
+use inputdata,only:Ep,nbeads,vtot
+implicit none
+integer::i,j,k
+vtot=0.0d0
+do i=1,nbeads
+	vtot=vtot+Ep(i)
+enddo
+end subroutine get_total_Ep
+
+subroutine read_initqp()
+use inputdata,only:initq,inito,natoms,nbeads,numtraj
+implicit none
+integer :: i,j,k,n
+open(300,file="initqp.dat")
+read(300,*)
+do n=1,numtraj
+	do j=1,natoms
+		do k=1,nbeads
+			read(300,'(6f18.10)')initq(:,j,k,n),initp(:,j,k,n)
+		enddo
+	enddo
+	read(300,*)
+enddo
+close(300)		
+end subroutine read_initqp
+
+
+subroutine init_random_seed()
+integer :: i,n,clock
+integer*4, allocatable:: seed(:)
+call random_seed(size=n)
+allocate (seed(n))
+
+call system_clock(count=clock)
+
+seed=clock + 37*(/(i-1,i=1,n)/)
+
+call random_seed(put=seed)
+
+eallocate(seed)
+end subroutine init_random_seed
+
+
 
 
